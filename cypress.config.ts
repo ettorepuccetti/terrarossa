4c384557e86eff2f22d6429db49fd09f67ed1fbe
Ettore Puccetti <puccio-94@live.it>
2023-07-02 13:46:17 +0200

    WIP cypress and docker integration

---
+++ cypress.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    baseUrl: "http://localhost:3000",
+    env: {
+      
+    },
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: "next",
+      bundler: "webpack",
+    },
+  },
+});
