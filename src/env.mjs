c82d879dd89d46672df9e7ed3fa5b5263938ad76
Ettore Puccetti <puccio-94@live.it>
2023-08-31 01:30:37 +0200

    no prebuild on vercel deploy

--- src/env.mjs
+++ src/env.mjs
@@ -19,12 +19,6 @@ const server = z.object({
     process.env.VERCEL ? z.string().min(1) : z.string().url()
   ),
   // Add `.min(1) on ID and SECRET if you want to make sure they're not empty
-  // FACEBOOK_CLIENT_ID: z.string(),
-  // FACEBOOK_CLIENT_SECRET: z.string(),
-  // GOOGLE_CLIENT_ID: z.string(),
-  // GOOGLE_CLIENT_SECRET: z.string(),
-  // GITHUB_CLIENT_ID: z.string(),
-  // GITHUB_CLIENT_SECRET: z.string(),
   AUTH0_CLIENT_ID: z.string(),
   AUTH0_CLIENT_SECRET: z.string(),
   AUTH0_ISSUER: z.string(),
@@ -49,16 +43,9 @@ const processEnv = {
   NODE_ENV: process.env.NODE_ENV,
   NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
   NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-  // FACEBOOK_CLIENT_ID: process.env.FACEBOOK_CLIENT_ID,
-  // FACEBOOK_CLIENT_SECRET: process.env.FACEBOOK_CLIENT_SECRET,
-  // GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
-  // GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-  // GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
-  // GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
   AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
   AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET,
   AUTH0_ISSUER: process.env.AUTH0_ISSUER,
-  // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
 };
 
 // Don't touch the part below
