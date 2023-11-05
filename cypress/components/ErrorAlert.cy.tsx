7d847465488694656326deb51c518ab61f7b10ad
Ettore Puccetti <puccio-94@live.it>
2023-11-06 00:59:20 +0100

    fix errorAlert test

--- cypress/components/ErrorAlert.cy.tsx
+++ cypress/components/ErrorAlert.cy.tsx
@@ -1,15 +1,17 @@
 import { TRPCClientError } from "@trpc/client";
 import ErrorAlert from "~/components/ErrorAlert";
+import { mountWithContexts } from "./_constants";
 
 describe("<ErrorAlert />", () => {
   it("renders", () => {
     // see: https://on.cypress.io/mounting-react
     const errorMessage = "test";
-    cy.mount(
+    mountWithContexts(
       <ErrorAlert
         error={new TRPCClientError(errorMessage)}
         onClose={() => null}
       />,
+      null,
     );
     cy.get(".MuiAlert-message").should("contain", errorMessage);
   });
