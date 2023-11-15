0e0d42884c9336768042014a52a63b96e415988a
Ettore Puccetti <puccio-94@live.it>
2023-11-15 01:50:31 +0100

    integrate accelerate

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,14 +1,8 @@
 import { PrismaClient } from "@prisma/client";
-
+import { withAccelerate } from "@prisma/extension-accelerate";
 import { env } from "~/env.mjs";
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
-
 export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    log:
-      env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
-  });
-
-if (env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+  env.NEXT_PUBLIC_APP_ENV === "production"
+    ? new PrismaClient({ log: ["error"] }).$extends(withAccelerate())
+    : new PrismaClient({ log: ["error", "query", "warn"] });
