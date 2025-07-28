8ed571144d90745248daa07d007920ce152d54ea
Ettore Puccetti <puccio-94@live.it>
2025-07-28 12:08:48 +0200

    FIX: disable pino logger (temp)

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,38 +1,47 @@
 import { useSession } from "next-auth/react";
-import pino from "pino";
-import { logflarePinoVercel } from "pino-logflare";
 import { env } from "~/env.mjs";
 
 // create pino-logflare console stream for serverless functions and send function for browser logs
 // Browser logs and serverless function logs are going to: https://logflare.app/sources/29358
 
-const LOGFLARE_SOURCE_ID_DEV = "8445833b-9fd1-47d8-b9b1-5948d7c88b42";
+// const LOGFLARE_SOURCE_ID_DEV = "8445833b-9fd1-47d8-b9b1-5948d7c88b42";
 
-const { stream, send } = logflarePinoVercel({
-  apiKey: env.NEXT_PUBLIC_LOGFLARE_API_KEY,
-  sourceToken:
-    env.NEXT_PUBLIC_APP_ENV === "production"
-      ? env.NEXT_PUBLIC_LOGFLARE_SOURCE_ID
-      : LOGFLARE_SOURCE_ID_DEV,
-});
+// const { stream, send } = logflarePinoVercel({
+//   apiKey: env.NEXT_PUBLIC_LOGFLARE_API_KEY,
+//   sourceToken:
+//     env.NEXT_PUBLIC_APP_ENV === "production"
+//       ? env.NEXT_PUBLIC_LOGFLARE_SOURCE_ID
+//       : LOGFLARE_SOURCE_ID_DEV,
+// });
 
 // create pino logger
-export const loggerInternal = pino(
-  {
-    browser: {
-      transmit: {
-        level: "info",
-        send: send,
-      },
-    },
-    base: {
-      env: process.env.NODE_ENV,
-      vercelEnv: process.env.VERCEL_ENV,
-      revision: process.env.VERCEL_GITHUB_COMMIT_SHA,
-    },
+// export const loggerInternal = pino(
+//   {
+//     browser: {
+//       transmit: {
+//         level: "info",
+//         send: send,
+//       },
+//     },
+//     base: {
+//       env: process.env.NODE_ENV,
+//       vercelEnv: process.env.VERCEL_ENV,
+//       revision: process.env.VERCEL_GITHUB_COMMIT_SHA,
+//     },
+//   },
+//   stream,
+// );
+
+export const loggerInternal = {
+  child: ({}) => {
+    return {
+      debug: console.debug,
+      info: console.log,
+      error: console.error,
+      warn: console.warn,
+    };
   },
-  stream,
-);
+};
 
 export const useLogger = (context: { [key: string]: string | undefined }) => {
   const { data: session } = useSession();
