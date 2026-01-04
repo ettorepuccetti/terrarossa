aa0564b10ab3f1d75f44f0f2359ac7433936b876
Ettore Puccetti <puccio-94@live.it>
2026-01-04 17:27:57 +0100

    specify system node version for cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,14 @@
 import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 import { defineConfig } from "cypress";
 import dotenv from "dotenv";
+import path from "path";
 import { PrismaClient } from "~/generated/prisma/client";
 
 dotenv.config();
 
-const adapter = new PrismaBetterSqlite3({ url: `file://prisma/dev.db` });
+const dbPath = path.join(process.cwd(), "prisma", "dev.db");
+const adapter = new PrismaBetterSqlite3({ url: `file:${dbPath}` });
+
 const prisma = new PrismaClient({ adapter });
 
 export default defineConfig({
@@ -22,6 +25,7 @@ export default defineConfig({
     env: {
       AUTH0_ISSUER: process.env.AUTH0_ISSUER,
     },
+    resolvedNodeVersion: "system",
     setupNodeEvents(on, config) {
       on("before:browser:launch", (browser, launchOptions) => {
         if (browser.family === "chromium" && browser.name !== "electron") {
